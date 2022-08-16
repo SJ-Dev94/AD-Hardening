f4519cb8580f00302b72277e996adfe8b2cf951f
sj-dev94 <sethjonesdevelopment@gmail.com>
2022-08-16 13:55:31 -0400

    Testing creds

--- Information Gathering/SIDresolver.ps1
+++ Information Gathering/SIDresolver.ps1
@@ -2,3 +2,5 @@ $SID ='ENTER SID HERE'
 $SIDObject = New-Object System.Security.Principal.SecurityIdentifier($SID)
 $UserObject = $SIDObject.Translate([System.Security.Principal.NTAccount])
 Write-Host "Username: " $UserObject.Value
+
+#testing
