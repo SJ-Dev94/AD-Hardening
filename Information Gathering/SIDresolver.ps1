33fe5e9db036cd2df0fe911706b295f1bb945252
sj-dev94 <sethjonesdevelopment@gmail.com>
2022-08-16 13:56:23 -0400

    test

--- Information Gathering/SIDresolver.ps1
+++
@@ -1,6 +0,0 @@
-$SID ='ENTER SID HERE'
-$SIDObject = New-Object System.Security.Principal.SecurityIdentifier($SID)
-$UserObject = $SIDObject.Translate([System.Security.Principal.NTAccount])
-Write-Host "Username: " $UserObject.Value
-
-#testing
