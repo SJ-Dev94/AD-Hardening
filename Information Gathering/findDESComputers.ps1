1fec1574c00c9d401570770acb6ae7e10d620cf7
sj-dev94 <sethjonesdevelopment@gmail.com>
2022-08-31 17:10:06 -0400

    added some context and reference to the findDES script}

--- Information Gathering/findDESComputers.ps1
+++ Information Gathering/findDESComputers.ps1
@@ -1,1 +1,6 @@
+#required reading for context https://docs.microsoft.com/en-us/archive/blogs/openspecification/msds-supportedencryptiontypes-episode-1-computer-accounts
+#required reading: https://techcommunity.microsoft.com/t5/core-infrastructure-and-security/decrypting-the-selection-of-supported-kerberos-encryption-types/ba-p/1628797
+#type "28" is "RC4","AES128","AES256" - we want this
+#DES must be removed
+
 get-adcomputer -properties msDS-SupportedEncryptionTypes -filter *
