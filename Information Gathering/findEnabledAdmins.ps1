bcb0d60d2b6ddbce8d76156381b94e31ce0b5c00
sj-dev94 <sethjonesdevelopment@gmail.com>
2022-09-06 11:57:13 -0400

    smol change

--- Information Gathering/findEnabledAdmins.ps1
+++ Information Gathering/findEnabledAdmins.ps1
@@ -1,9 +1,9 @@
 function findEnabledAdmins {
-    Get-ADGroupMember -Server "fwic.local" -Identity "Domain Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\enabledAdmins.csv
+    Get-ADGroupMember -Server "domain here" -Identity "Domain Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\enabledAdmins.csv
 
-    Get-ADGroupMember -Server "fwic.local" -Identity "Schema Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\schemaAdmins.csv
+    Get-ADGroupMember -Server "domain here" -Identity "Schema Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\schemaAdmins.csv
 
-    Get-ADGroupMember -Server "fwic.local" -Identity "Enterprise Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\enterpriseAdmins.csv
+    Get-ADGroupMember -Server "domain here" -Identity "Enterprise Admins" -Recursive | Get-ADUser | Select Name, Enabled | Export-Csv -Path .\enterpriseAdmins.csv
 }
 
 findEnabledAdmins
